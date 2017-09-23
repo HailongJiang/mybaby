64fc7941772da57741f89c6b38c7d80bd78eabe3
HailongJiang <32008689+HailongJiang@users.noreply.github.com>
2017-09-23 21:30:27 +0800

    mybaby
    growth record

---
+++ mybaby/js/mybaby.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+	var swiper = new Swiper('.swiper-container', {
+    	    // pagination: '.swiper-pagination',
+    	    // paginationClickable: true,
+    	    direction: 'vertical',
+    	    onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+   			swiperAnimateCache(swiper); //隐藏动画元素 
+   			swiperAnimate(swiper); //初始化完成开始动画
+ 			}, 
+ 			onSlideChangeEnd: function(swiper){ 
+   			swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+  			} 
+    	});
+}
